66273ee45ab9e233fb2bf8005edbb04dc7cfcad6
hha046 <harald.hanssen@hnikt.no>
2018-11-02 15:49:55 +0100

    #9 The FhirStarterSettingsHelper and FhirStarterConfig.cs must be tested.

--- src/FhirStarter.Inferno.DotNetCore/FhirStarter.Inferno.WebAPI/Config/FhirStarterConfig.cs
+++ src/FhirStarter.Inferno.DotNetCore/FhirStarter.Inferno.WebAPI/Config/FhirStarterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FhirStarter.Bonfire.DotNetCore.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,6 +19,9 @@ namespace FhirStarter.Inferno.WebAPI.Config
             RegisterServices(app, fhirStarterSettings);
         }
 
+        #region Assembly
+
+        
 
         private static void RegisterServices(IApplicationBuilder app, IConfigurationRoot fhirStarterSettings)
         {
@@ -30,16 +34,62 @@ namespace FhirStarter.Inferno.WebAPI.Config
                 new TypeInitializer(true, fhirStructureDefinition, nameof(AbstractStructureDefinitionService))
             };
 
-            //var serviceAssemblies = AssemblyLoaderHelper.GetFhirServiceAssemblies();
-            //foreach (var asm in serviceAssemblies)
-            //{
-            //    var types = asm.GetTypes();
-            //    foreach (var classType in asm.GetTypes())
-            //    {
-            //        BindIFhirServices(kernel, serviceTypes, classType);
-            //    }
-            //}
+            var fhirServiceAssemblies = FhirStarterSettingsHelper.GetFhirServiceAssemblies(fhirStarterSettings);
+
+            foreach (var asm in fhirServiceAssemblies)
+            {
+                var types = asm.GetTypes();
+                foreach (var classType in asm.GetTypes())
+                {
+                    BindIFhirServices(app, serviceTypes, classType);
+                }
+            }
         }
+
+        private static void BindIFhirServices(IApplicationBuilder app, List<TypeInitializer> serviceTypes, Type classType)
+        {
+            var serviceType = FindType(serviceTypes, classType);
+            if (serviceType != null)
+            {
+                if (serviceType.Name.Equals(nameof(IFhirService)))
+                {
+                    var instance = (IFhirService)Activator.CreateInstance(classType);
+                    //app.Bind<IFhirService>().ToConstant(instance);
+                    //_amountOfInitializedIFhirServices++;
+                }
+                //else if (serviceType.Name.Equals(nameof(IFhirMockupService)))
+                //{
+                //    var instance = (IFhirMockupService)Activator.CreateInstance(classType);
+                //    kernel.Bind<IFhirMockupService>().ToConstant(instance);
+                //    _amountOfInitializedIFhirMockupServices++;
+                //}
+                //else if (serviceType.Name.Equals(nameof(AbstractStructureDefinitionService)))
+                //{
+                //    var structureDefinitionService = (AbstractStructureDefinitionService)Activator.CreateInstance(classType);
+                //    kernel.Bind<AbstractStructureDefinitionService>().ToConstant(structureDefinitionService);
+                //    var validator = structureDefinitionService.GetValidator();
+                //    if (validator != null)
+                //    {
+                //        var profileValidator = new ProfileValidator(validator);
+                //        kernel.Bind<ProfileValidator>().ToConstant(profileValidator);
+                //    }
+                //    _amountOfIFhirStructureDefinitionsInitialized++;
+                //}
+            }
+        }
+
+        private static TypeInitializer FindType(List<TypeInitializer> serviceTypes, Type classType)
+        {
+            foreach (var service in serviceTypes)
+            {
+                if (service.ServiceType.IsAssignableFrom(classType) && !classType.IsInterface && !classType.IsAbstract)
+                    return service;
+            }
+            return null;
+        }
+
+        #endregion Assembly
+
 
         private static void SetupLogging(ILoggerFactory loggerFactory)
         {
---
+++ src/FhirStarter.Inferno.DotNetCore/FhirStarter.Inferno.WebAPI/Config/FhirStarterSettingsHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace FhirStarter.Inferno.WebAPI.Config
+{
+    public static class FhirStarterSettingsHelper
+    {
+        private const string FhirStarterSettingsFhirServiceAssemblies = "FhirStarterSettings:FhirServiceAssemblies";
+
+        public static ICollection<Assembly> GetFhirServiceAssemblies(IConfigurationRoot fhirStarterSettings)
+        {
+            var assemblyNames = GetArrayFromSettings(fhirStarterSettings);
+            if (assemblyNames.Any())
+            {
+                return  assemblyNames.Select(assemblyName => Assembly.Load(assemblyName.Trim())).ToList();
+            }
+            throw new ArgumentException($"The setting {FhirStarterSettingsFhirServiceAssemblies} in FhirStarterSettings.json is empty");
+        }
+
+        private static ICollection<string> GetArrayFromSettings(IConfigurationRoot fhirStarterSettings)
+        {
+            var assemblyNames = fhirStarterSettings.GetValue<string[]>(FhirStarterSettingsFhirServiceAssemblies);
+            if (assemblyNames != null && assemblyNames.Any())
+            {
+                return assemblyNames;
+            }
+            return new List<string>();
+        }
+    }
+}
