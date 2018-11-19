aa062629e9f55037735b6799ebd979b1462bf20b
hha046 <harald.hanssen@hnikt.no>
2018-11-19 15:16:48 +0100

    #13 Removed comments in the old library

--- src/FhirStarter.Bonfire.DotNetCore/SparkEngine/ExceptionHandling/FhirErrorMessageHandler.cs
+++ src/FhirStarter.Bonfire.DotNetCore/SparkEngine/ExceptionHandling/FhirErrorMessageHandler.cs
@@ -1,10 +1,11 @@
 ﻿/* 
- * Copyright (c) 2014, Furore (info@furore.com) and contributors
+ * Copyright (c) 2018, Firely (info@furore.com) and contributors
  * See the file CONTRIBUTORS for details.
  * 
  * This file is licensed under the BSD 3-Clause license
  * available at https://raw.github.com/furore-fhir/spark/master/LICENSE
  */
+
 
 using System.Net.Http;
 using System.Threading;
@@ -16,7 +17,6 @@ namespace FhirStarter.Bonfire.DotNetCore.SparkEngine.ExceptionHandling
 {
     public class FhirErrorMessageHandler : DelegatingHandler
     {
-
         // the error handling has been changed, look into this
         protected override async Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request, CancellationToken cancellationToken)
@@ -25,11 +25,8 @@ namespace FhirStarter.Bonfire.DotNetCore.SparkEngine.ExceptionHandling
 
             if (!response.IsSuccessStatusCode)
             {
-                //var content = response.Content as ObjectContent;
-                var content = response.Content as ObjectContent;
-                if (content != null && content.ObjectType == typeof (HttpError))
+                if (response.Content is ObjectContent content && content.ObjectType == typeof (HttpError))
                 {
-                    //  var outcome = new OperationOutcome().AddError(response.ReasonPhrase);
                     var issue = new OperationOutcome.IssueComponent
                     {
                         Details = new CodeableConcept(nameof(HttpError), nameof(HttpError), response.ReasonPhrase)
